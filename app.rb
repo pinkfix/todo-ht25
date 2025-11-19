ed5db3d6e9820efe2f19fa45bf5f600f6b519611
pinkfix <robin@willim.se>
2025-11-19 16:21:00 +0100

    Made the base page with CRUD fully implemented

--- app.rb
+++ app.rb
@@ -5,20 +5,57 @@ require 'sinatra/reloader'
 
 
 
-# Funktion för att prata med databasen
-# Exempel på användning: db.execute('SELECT * FROM fruits')
-def db
-  return @db if @db
-
-  @db = SQLite3::Database.new(DB_PATH)
-  @db.results_as_hash = true
-
-  return @db
-end
 
 # Routen /
-get '/' do
+get('/') do
+    db = SQLite3::Database.new('db/todos.db') # Öppnar datanas
+    db.results_as_hash = true #Begär hash
+
+    @postit = db.execute("SELECT * FROM todos") # Tar ut datan, förvarar i @animals
+
     slim(:index)
 end
 
 
+post('/add') do
+  namnet = params[:name]
+  desc = params[:description]
+  db = SQLite3::Database.new('db/todos.db') # Öppnar datanas
+  db.results_as_hash = true #Begär hash
+
+  @postit = db.execute("INSERT INTO todos (name, description) VALUES (?,?)",[ namnet, desc]) # Tar ut datan, förvarar i @animals
+
+  redirect('/')
+end
+
+
+get('/edit/:id') do
+  db = SQLite3::Database.new('db/todos.db') # Öppnar datanas
+  db.results_as_hash = true #Begär hash
+  id = params[:id]
+
+  @postitnote = db.execute("SELECT * FROM todos WHERE id = ?", id).first
+
+  slim(:edit)
+end
+
+post('/edit/:id') do
+  id = params[:id]
+  name = params[:name]
+  desc = params[:description]
+  db = SQLite3::Database.new('db/todos.db') # Öppnar datanas
+  db.results_as_hash = true #Begär hash
+
+  db.execute("UPDATE todos SET name = ?, description = ? WHERE id = ?", [name, desc, id])
+  redirect('/')
+end
+
+
+post('/delete/:id') do
+  id = params[:id]
+  db = SQLite3::Database.new('db/todos.db') # Öppnar datanas
+  db.results_as_hash = true #Begär hash
+
+  db.execute("DELETE FROM todos WHERE id = ?", id)
+  redirect('/')
+end
